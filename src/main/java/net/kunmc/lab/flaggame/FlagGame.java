39a905b6257a03045245f14f66785030a92469a6
Tomo-dq <tomomodq69@gmail.com>
2021-02-11 11:49:59 +0900

    修正

--- src/main/java/net/kunmc/lab/flaggame/FlagGame.java
+++ src/main/java/net/kunmc/lab/flaggame/FlagGame.java
@@ -171,7 +171,7 @@ public final class FlagGame extends JavaPlugin {
                 return Stream.of("speed", "difficulty").filter(e -> e.startsWith(args[1])).collect(Collectors.toList());
             }
             if(args.length == 3 && args[1].equals("speed")) {
-                return Stream.of("0.5", "0.6", "0.7", "0.8", "0.9", "1.0", "1,1", "1.2", "1.3", "1.4", "1.5", "1.6", "1.7", "1.8", "1.9", "2.0", "2.1", "2.2", "2.3", "2.4", "2.5")
+                return Stream.of("0.5", "0.6", "0.7", "0.8", "0.9", "1.0", "1.1", "1.2", "1.3", "1.4", "1.5", "1.6", "1.7", "1.8", "1.9", "2.0", "2.1", "2.2", "2.3", "2.4", "2.5")
                         .filter(e -> e.startsWith(args[2])).collect(Collectors.toList());
             }
             if(args.length == 3 && args[1].equals("difficulty")) {
